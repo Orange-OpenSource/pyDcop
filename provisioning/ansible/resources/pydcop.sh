de2e38c0e981ade76a0e42ff5750cf48455e39a9
Pierre Rust <pierre.rust@orange.com>
2018-06-14 13:55:00 +0200

    Fix pydcop service script

--- provisioning/ansible/resources/pydcop.sh
+++ provisioning/ansible/resources/pydcop.sh
@@ -3,7 +3,7 @@
 export DISPLAY=:0
 
 # Launch pydcop
-python3 pydcop agent -n {{agt_name}} -p 9001 --uiport 10001  --orchestrator {{controller_proxy_addr}}:9000 &
+pydcop agent -n {{agt_name}} -p 9001 --uiport 10001  --orchestrator {{controller_proxy_addr}}:9000 &
 
 #launch ui web server
 cd ~/pyDcop-ui/dist
