53235f9a70f7150c0e008bd82d07cb083e1241ef
Pierre Rust <pierre.rust@orange.com>
2018-07-02 15:05:09 +0200

    Simple event Bus
    useful to collect events without introducing too much coupling between
    different parts of the lib.

---
+++ pydcop/infrastructure/Events.py
@@ -0,0 +1,98 @@
+# BSD-3-Clause License
+#
+# Copyright 2017 Orange
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its contributors
+#    may be used to endorse or promote products derived from this software
+#    without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+
+"""
+very simple event-bux mecanism.
+
+"""
+from collections import defaultdict
+from functools import wraps
+from typing import Callable
+
+
+class EventDispatcher(object):
+    """
+    A very simple event dispatcher.
+
+    """
+
+    def __init__(self):
+        self._cbs = defaultdict(lambda: [])
+
+    def send(self, topic, evt):
+        for cb in self._cbs[topic]:
+            cb(topic, evt)
+        eligibles = [cbs for s_topic, cbs  in self._cbs.items()
+                    if s_topic[-1] == '*' and topic.startswith(s_topic[:-1])]
+        for cbs in eligibles:
+            for cb in cbs:
+                cb(topic, evt)
+
+    def subscribe(self, topic: str, cb: Callable):
+        """
+        Register a call back to topic.
+
+        Parameters
+        ----------
+        topic: str
+            a topic
+        cb:
+            a callback
+
+        Returns
+        -------
+        callable:
+            The registered callback.
+        """
+        self._cbs[topic].append(cb)
+        return cb
+
+
+    def unsubscribe(self, cb: Callable, topic: str=None):
+
+        if topic is None:
+            for s_topic,  s_cbs in self._cbs.copy().items():
+                if cb in s_cbs:
+                    s_cbs.remove(cb)
+        # else:
+
+    def reset(self):
+        self._cbs.clear()
+
+
+
+event_bus = EventDispatcher()
+
+buses = defaultdict(lambda : EventDispatcher())
+
+def get_bus(name: str):
+    return buses[name]
+
---
+++ tests/unit/test_events.py
@@ -0,0 +1,94 @@
+from unittest.mock import MagicMock
+
+import pytest
+
+
+@pytest.fixture
+def event_bus():
+    from pydcop.infrastructure.Events import event_bus
+    event_bus.reset()
+    return event_bus
+
+def test_simple_send(event_bus):
+
+    cb = MagicMock()
+
+    event_bus.subscribe('evt-name', cb)
+
+    event_bus.send('evt-name', 42)
+    cb.assert_called_once_with('evt-name', 42)
+
+
+def test_cb_not_called_when_not_subscribed(event_bus):
+
+    cb = MagicMock()
+    event_bus.send('another', 48)
+    cb.assert_not_called()
+
+
+def test_do_not_receive_evt_once_unsubscribed(event_bus):
+    cb = MagicMock()
+
+    event_bus.subscribe('evts', cb)
+
+    event_bus.send('evts', 42)
+
+    cb.assert_called_once_with('evts', 42)
+
+    cb.reset_mock()
+    event_bus.unsubscribe(cb)
+
+    event_bus.send('evts', 43)
+    cb.assert_not_called()
+
+def test_unsubscribe_from_single_topic(event_bus):
+
+    cb1 = event_bus.subscribe('evts', MagicMock())
+    cb2 = event_bus.subscribe('evts', MagicMock())
+
+    event_bus.send('evts', 42)
+
+    cb1.assert_called_once_with('evts', 42)
+    cb2.assert_called_once_with('evts', 42)
+
+    cb1.reset_mock()
+    cb2.reset_mock()
+    event_bus.unsubscribe(cb2)
+
+    event_bus.send('evts', 43)
+    cb1.assert_called_once_with('evts', 43)
+    cb2.assert_not_called()
+
+
+def test_several_subscribers(event_bus):
+
+    cb1 = event_bus.subscribe('evt-name', MagicMock())
+    cb2 = event_bus.subscribe('evt-name', MagicMock())
+    cb3 = event_bus.subscribe('evt-name', MagicMock())
+
+    event_bus.send('evt-name', 42)
+
+    cb1.assert_called_once_with('evt-name', 42)
+    cb2.assert_called_once_with('evt-name', 42)
+    cb3.assert_called_once_with('evt-name', 42)
+
+
+def test_receive_evt_from_sub_topics(event_bus):
+
+    cb1 = event_bus.subscribe('a.b.*', MagicMock())
+
+    # Event on sub-topic : must be received
+    event_bus.send('a.b.c', 42)
+    cb1.assert_called_once_with('a.b.c', 42)
+
+    # Event on another topic : must not be received
+    cb1.reset_mock()
+    event_bus.send('a.another.d.f', 42)
+    cb1.assert_not_called()
+
+    # Event on another topic : must not be received
+    cb1.reset_mock()
+    event_bus.send('a.bb.f', 42)
+    cb1.assert_not_called()
+
+
