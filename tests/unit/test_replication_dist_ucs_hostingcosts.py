4dfc016f0f534b6670365f518d3980d4647eef75
Pierre Rust <pierre.rust@orange.com>
2018-10-10 11:21:47 +0200

    Unit Test for msg serialization

--- tests/unit/test_replication_dist_ucs_hostingcosts.py
+++ tests/unit/test_replication_dist_ucs_hostingcosts.py
@@ -27,59 +27,87 @@
 # CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
+import json
 
-
-from unittest.mock import MagicMock
-
-from pydcop.replication.dist_ucs_hostingcosts import UCSReplication
+from pydcop.replication.dist_ucs_hostingcosts import UCSReplicateMessage
 from pydcop.replication.dist_ucs_hostingcosts import ReplicationTracker
+from pydcop.replication.path_utils import Path, PathsTable
+from pydcop.utils.simple_repr import simple_repr, from_repr
 
 
 def test_tracker_add_computations():
     tracker = ReplicationTracker()
-    tracker.add(['c1', 'c2', 'c3'])
+    tracker.add(["c1", "c2", "c3"])
 
     assert not tracker.is_empty()
-    assert set(tracker.in_progress()) == {'c1', 'c2', 'c3'}
+    assert set(tracker.in_progress()) == {"c1", "c2", "c3"}
 
 
 def test_tracker_add_computation_several_time_must_increase_count():
     tracker = ReplicationTracker()
-    tracker.add(['c1', 'c2', 'c3'])
-    tracker.add(['c1', 'c3'])
+    tracker.add(["c1", "c2", "c3"])
+    tracker.add(["c1", "c3"])
 
     assert not tracker.is_empty()
-    assert tracker.replicating['c1'] == 2
-    assert tracker.replicating['c2'] == 1
-    assert tracker.replicating['c3'] == 2
+    assert tracker.replicating["c1"] == 2
+    assert tracker.replicating["c2"] == 1
+    assert tracker.replicating["c3"] == 2
 
 
 def test_tracker_remove_computation_must_decrease_count():
     tracker = ReplicationTracker()
-    tracker.add(['c1', 'c2', 'c3'])
-    tracker.add(['c1', 'c2', 'c3'])
-    tracker.remove(['c1', 'c3'])
+    tracker.add(["c1", "c2", "c3"])
+    tracker.add(["c1", "c2", "c3"])
+    tracker.remove(["c1", "c3"])
 
     assert not tracker.is_empty()
-    assert not tracker.is_done('c1')
-    assert not tracker.is_done('c2')
-    assert not tracker.is_done('c3')
-    assert tracker.replicating['c1'] == 1
-    assert tracker.replicating['c2'] == 2
-    assert tracker.replicating['c3'] == 1
+    assert not tracker.is_done("c1")
+    assert not tracker.is_done("c2")
+    assert not tracker.is_done("c3")
+    assert tracker.replicating["c1"] == 1
+    assert tracker.replicating["c2"] == 2
+    assert tracker.replicating["c3"] == 1
 
 
 def test_tracker_when_reaching_0_computation_is_removed():
     tracker = ReplicationTracker()
-    tracker.add(['c1', 'c2', 'c3'])
-    tracker.remove(['c1', 'c3'])
+    tracker.add(["c1", "c2", "c3"])
+    tracker.remove(["c1", "c3"])
 
-    assert tracker.is_done('c1')
-    assert tracker.replicating['c2'] == 1
-    assert tracker.is_done('c3')
+    assert tracker.is_done("c1")
+    assert tracker.replicating["c2"] == 1
+    assert tracker.is_done("c3")
 
-    tracker.remove(['c2'])
+    tracker.remove(["c2"])
     assert tracker.is_empty()
 
 
+def test_serialization_message():
+    p1 = Path("1", "2", "2")
+    table = PathsTable({p1: 3})
 
+    msg = UCSReplicateMessage("msg_type", 0, 0, p1, table, ["1", "2"], None, 5, 2, [])
+
+    r = simple_repr(msg)
+    assert r
+    # simply taking a simple_repr does not ensure that the json serialization is valid
+    json_msg = json.dumps(r)
+    assert json_msg
+
+
+def test_unserialization_msg():
+    p1 = Path("1", "2", "2")
+    table = PathsTable({p1: 3})
+
+    msg = UCSReplicateMessage("msg_type", 0, 0, p1, table, ["1", "2"], None, 5, 2, [])
+
+    r = simple_repr(msg)
+    assert r
+    # simply taking a simple_repr does not ensure that the json serialization is valid
+    json_msg = json.dumps(r)
+    loaded = json.loads(json_msg)
+
+    obtained = from_repr(loaded)
+    assert obtained
+
+    assert obtained == msg
