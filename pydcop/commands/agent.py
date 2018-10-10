9f56e726ffb4d83ef2803a0c4f09ab9bac0a4f9f
Pierre Rust <pierre.rust@orange.com>
2018-10-10 14:53:23 +0200

    CLI: add replication and capacity to agent cmd

--- pydcop/commands/agent.py
+++ pydcop/commands/agent.py
@@ -115,6 +115,8 @@ Options
   runtime.
 
 
+
+
 Note that the agent's port defaults to 9001 to avoid conflicts with the
 orchestrator, whose port defaults to 9000.
 
@@ -140,90 +142,148 @@ from pydcop.dcop.objects import AgentDef
 from pydcop.infrastructure.orchestratedagents import OrchestratedAgent
 from pydcop.infrastructure.communication import HttpCommunicationLayer
 
-logger = logging.getLogger('pydcop.cli.agent')
+logger = logging.getLogger("pydcop.cli.agent")
 force_stopped = False
 agents = []
 
 
 def set_parser(subparsers):
 
-    parser = subparsers.add_parser('agent',
-                                   help='Run one or several standalone agents')
+    parser = subparsers.add_parser("agent", help="Run one or several standalone agents")
     parser.set_defaults(func=run_cmd)
     parser.set_defaults(on_force_exit=on_force_exit)
 
-    parser.add_argument('-n', '--names', type=str, nargs='+',
-                        help='The name of the agent(s). This must match the '
-                             'name of the agents expected by the '
-                             'orchestrator.')
-    parser.add_argument('--address', type=str, default=None,
-                        help="IP address the orchestrator will listen on. If "
-                             "not given we try to use the primary IP address.")
-    parser.add_argument('-p', '--port', type=int, default=9001,
-                        help='The port on which the agent will listen for '
-                             'messages. If several agents are started '
-                             '(when giving several names) this port is used '
-                             'for the first agent and incremented for each '
-                             'following agent.')
-    parser.add_argument('-o', '--orchestrator', type=str,
-                        help='The address of the orchestrator <ip>[:<port>]')
+    parser.add_argument(
+        "-n",
+        "--names",
+        type=str,
+        nargs="+",
+        help="The name of the agent(s). This must match the "
+        "name of the agents expected by the "
+        "orchestrator.",
+    )
+    parser.add_argument(
+        "--address",
+        type=str,
+        default=None,
+        help="IP address the orchestrator will listen on. If "
+        "not given we try to use the primary IP address.",
+    )
+    parser.add_argument(
+        "-p",
+        "--port",
+        type=int,
+        default=9001,
+        help="The port on which the agent will listen for "
+        "messages. If several agents are started "
+        "(when giving several names) this port is used "
+        "for the first agent and incremented for each "
+        "following agent.",
+    )
+    parser.add_argument(
+        "-o",
+        "--orchestrator",
+        type=str,
+        help="The address of the orchestrator <ip>[:<port>]",
+    )
 
-    parser.add_argument('-i', '--uiport', type=int, default=None,
-                        help='The port on which the ui-server will be listening'
-                             ' (same behavior as``--port`` when starting '
-                             'several agents). If not given, no ui-server will '
-                             'be started for these/this agent(s).')
-    parser.add_argument('--restart', action='store_true', default=False,
-                        help='When setting this flag, agent(s) will restarted'
-                             'when when they have all stopped. Useful when '
-                             'running `pydcop agent` as daemon on a remote '
-                             'machine.')
-    parser.add_argument('--delay', default=None, type=float,
-                        help='an optional delay between message delivery, '
-                             ' in second. This delay only applies to '
-                             'algorithm\'s messages and is useful when you '
-                             'want to observe (for example with the UI) the '
-                             'behavior of the algorithm at runtime')
+    parser.add_argument(
+        "-i",
+        "--uiport",
+        type=int,
+        default=None,
+        help="The port on which the ui-server will be listening"
+        " (same behavior as``--port`` when starting "
+        "several agents). If not given, no ui-server will "
+        "be started for these/this agent(s).",
+    )
+    parser.add_argument(
+        "--restart",
+        action="store_true",
+        default=False,
+        help="When setting this flag, agent(s) will restarted"
+        "when when they have all stopped. Useful when "
+        "running `pydcop agent` as daemon on a remote "
+        "machine.",
+    )
+    parser.add_argument(
+        "--delay",
+        default=None,
+        type=float,
+        help="an optional delay between message delivery, "
+        " in second. This delay only applies to "
+        "algorithm's messages and is useful when you "
+        "want to observe (for example with the UI) the "
+        "behavior of the algorithm at runtime",
+    )
+    parser.add_argument("--replication", default=False, action="store_true")
+    parser.add_argument("--capacity", default=100, type=int)
 
 
 def run_cmd(args):
     global agents
-    if ':' in args.orchestrator:
-        o_addr, o_port = args.orchestrator.split(':')
+    if ":" in args.orchestrator:
+        o_addr, o_port = args.orchestrator.split(":")
     else:
         o_addr, o_port = args.orchestrator, 9000
 
     names = list(args.names)
     if args.restart:
         while not force_stopped:
-            agents = start_agents(names, o_addr, int(o_port),
-                                  args.uiport, args.address, args.port,
-                                  args.delay)
+            agents = start_agents(
+                names,
+                o_addr,
+                int(o_port),
+                args.uiport,
+                args.address,
+                args.port,
+                args.delay,
+                args.replication,
+                args.capacity,
+            )
 
             # block until all agents have finished
             for agent in agents:
                 agent.join()
-            logger.info('All agents have stopped')
+            logger.info("All agents have stopped")
             if force_stopped:
                 break
-            logger.info('Wait before restarting')
+            logger.info("Wait before restarting")
             sleep(10)
 
     else:
-        agents = start_agents(names, o_addr, int(o_port),
-                              args.uiport, args.address, args.port, args.delay)
+        agents = start_agents(
+            names,
+            o_addr,
+            int(o_port),
+            args.uiport,
+            args.address,
+            args.port,
+            args.delay,
+            args.replication,
+            args.capacity,
+        )
 
 
 def on_force_exit(_, __):
-    print('FORCE EXIT')
+    print("FORCE EXIT")
     global agents, force_stopped
     force_stopped = True
     for agent in agents:
         agent.stop()
 
 
-def start_agents(names: List[str], o_addr, o_port, u_port, a_addr, a_port,
-                 delay):
+def start_agents(
+    names: List[str],
+    o_addr,
+    o_port,
+    u_port,
+    a_addr,
+    a_port,
+    delay,
+    replication,
+    capacity,
+):
     """
     Start orchestrated agents.
 
@@ -256,17 +316,30 @@ def start_agents(names: List[str], o_addr, o_port, u_port, a_addr, a_port,
     for a in names:
         if u_port:
             logger.info(
-                'Starting agent {} on port {} with ui-server on {}'.format(
-                    a, a_port, u_port))
+                "Starting agent {} on port {} with ui-server on {}".format(
+                    a, a_port, u_port
+                )
+            )
         else:
             logger.info(
-                'Starting agent {} on port {} without ui-server '.format(
-                    a, a_port))
+                "Starting agent {} on port {} without ui-server ".format(a, a_port)
+            )
 
         comm = HttpCommunicationLayer((a_addr, a_port))
-        agt_def = AgentDef(a)
-        agent = OrchestratedAgent(agt_def, comm, (o_addr, o_port),
-                                  ui_port=u_port, delay=delay)
+        agt_def = AgentDef(a, capacity=capacity)
+        if replication:
+            agent = OrchestratedAgent(
+                agt_def,
+                comm,
+                (o_addr, o_port),
+                ui_port=u_port,
+                delay=delay,
+                replication="dist_ucs_hostingcosts",
+            )
+        else:
+            agent = OrchestratedAgent(
+                agt_def, comm, (o_addr, o_port), ui_port=u_port, delay=delay
+            )
 
         agent.start()
         started_agents.append(agent)
