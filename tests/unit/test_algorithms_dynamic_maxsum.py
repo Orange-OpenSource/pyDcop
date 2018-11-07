33aa4f803b5e6f7c6d8a5ae6e0a7b12f92951903
Pierre Rust <pierre.rust@orange.com>
2018-11-07 11:15:37 +0100

    Fix Dyn Maxsum unit tests

--- tests/unit/test_algorithms_dynamic_maxsum.py
+++ tests/unit/test_algorithms_dynamic_maxsum.py
@@ -33,34 +33,34 @@ import types
 import unittest
 from unittest.mock import MagicMock
 
-from pydcop.algorithms.maxsum_dynamic import \
-    DynamicFunctionFactorComputation
+from pydcop.algorithms.maxsum_dynamic import DynamicFunctionFactorComputation
 from pydcop.dcop.objects import Variable
 from pydcop.dcop.relations import AsNAryFunctionRelation
-from pydcop.infrastructure.communication import InProcessCommunicationLayer, \
-    Messaging
+from pydcop.infrastructure.communication import InProcessCommunicationLayer, Messaging
 
 
 class DynamicFunctionFactorComputationTest(unittest.TestCase):
-
     def test_init(self):
 
         domain = list(range(10))
-        x1 = Variable('x1', domain)
-        x2 = Variable('x2', domain)
+        x1 = Variable("x1", domain)
+        x2 = Variable("x2", domain)
 
         @AsNAryFunctionRelation(x1, x2)
         def phi(x1_, x2_):
             return x1_ + x2_
 
-        f = DynamicFunctionFactorComputation(phi,  comp_def=MagicMock())
+        comp_def = MagicMock()
+        comp_def.algo.algo = "maxsum"
+        comp_def.algo.mode = "min"
+        f = DynamicFunctionFactorComputation(phi, comp_def=comp_def)
 
-        self.assertEqual(f.name, 'phi')
+        self.assertEqual(f.name, "phi")
 
     def test_change_function_name(self):
         domain = list(range(10))
-        x1 = Variable('x1', domain)
-        x2 = Variable('x2', domain)
+        x1 = Variable("x1", domain)
+        x2 = Variable("x2", domain)
 
         @AsNAryFunctionRelation(x1, x2)
         def phi(x1_, x2_):
@@ -70,17 +70,21 @@ class DynamicFunctionFactorComputationTest(unittest.TestCase):
         def phi2(x1_, x2_):
             return x1_ - x2_
 
-        f = DynamicFunctionFactorComputation(phi, msg_sender=MagicMock(),
-                                             comp_def=MagicMock())
+        comp_def = MagicMock()
+        comp_def.algo.algo = "maxsum"
+        comp_def.algo.mode = "min"
+        f = DynamicFunctionFactorComputation(
+            phi, msg_sender=MagicMock(), comp_def=comp_def
+        )
 
         f.change_factor_function(phi2)
 
-        self.assertEqual(f.name, 'phi')
+        self.assertEqual(f.name, "phi")
 
     def test_change_function_different_order(self):
         domain = list(range(10))
-        x1 = Variable('x1', domain)
-        x2 = Variable('x2', domain)
+        x1 = Variable("x1", domain)
+        x2 = Variable("x2", domain)
 
         @AsNAryFunctionRelation(x1, x2)
         def phi(x1_, x2_):
@@ -90,18 +94,22 @@ class DynamicFunctionFactorComputationTest(unittest.TestCase):
         def phi2(x2_, x1_):
             return x1_ - x2_
 
-        f = DynamicFunctionFactorComputation(phi, msg_sender=MagicMock(),
-                                             comp_def = MagicMock())
+        comp_def = MagicMock()
+        comp_def.algo.algo = "maxsum"
+        comp_def.algo.mode = "min"
+        f = DynamicFunctionFactorComputation(
+            phi, msg_sender=MagicMock(), comp_def=comp_def
+        )
         f.change_factor_function(phi2)
 
-        self.assertEqual(f.name, 'phi')
+        self.assertEqual(f.name, "phi")
 
     def test_change_function_wrong_dimensions_len(self):
 
         domain = list(range(10))
-        x1 = Variable('x1', domain)
-        x2 = Variable('x2', domain)
-        x3 = Variable('x3', domain)
+        x1 = Variable("x1", domain)
+        x2 = Variable("x2", domain)
+        x3 = Variable("x3", domain)
 
         @AsNAryFunctionRelation(x1, x2)
         def phi(x1_, x2_):
@@ -111,17 +119,20 @@ class DynamicFunctionFactorComputationTest(unittest.TestCase):
         def phi2(x1_, x2_, x3_):
             return x1_ - x2_ + x3_
 
-        f = DynamicFunctionFactorComputation(phi, comp_def=MagicMock())
-        # Monnkey patch post_msg method with dummy mock to avoid error:
+        comp_def = MagicMock()
+        comp_def.algo.algo = "maxsum"
+        comp_def.algo.mode = "min"
+        f = DynamicFunctionFactorComputation(phi, comp_def=comp_def)
+        # Monkey patch post_msg method with dummy mock to avoid error:
         f.post_msg = types.MethodType(lambda w, x, y, z: None, f)
 
         self.assertRaises(ValueError, f.change_factor_function, phi2)
 
     def test_change_function_wrong_dimensions_var(self):
         domain = list(range(10))
-        x1 = Variable('x1', domain)
-        x2 = Variable('x2', domain)
-        x3 = Variable('x3', domain)
+        x1 = Variable("x1", domain)
+        x2 = Variable("x2", domain)
+        x3 = Variable("x3", domain)
 
         @AsNAryFunctionRelation(x1, x2)
         def phi(x1_, x2_):
@@ -131,9 +142,12 @@ class DynamicFunctionFactorComputationTest(unittest.TestCase):
         def phi2(x1_, x3_):
             return x1_ + x3_
 
-        f = DynamicFunctionFactorComputation(phi, comp_def=MagicMock())
-        # Monnkey patch post_msg method with dummy mock to avoid error:
+        comp_def = MagicMock()
+        comp_def.algo.algo = "maxsum"
+        comp_def.algo.mode = "min"
+        f = DynamicFunctionFactorComputation(phi, comp_def=comp_def)
+
+        # Monkey patch post_msg method with dummy mock to avoid error:
         f.post_msg = types.MethodType(lambda w, x, y, z: None, f)
 
         self.assertRaises(ValueError, f.change_factor_function, phi2)
-
