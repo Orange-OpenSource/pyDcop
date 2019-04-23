b8979e5b52f2284867e182407795b98a20696163
Pierre Rust <pierre.rust@orange.com>
2019-04-23 17:07:27 +0200

    Improve consolidate command

--- pydcop/commands/consolidate.py
+++ pydcop/commands/consolidate.py
@@ -61,10 +61,21 @@ average
  takes a csv file
 
 """
+import csv
+import glob
 import logging
 import json
-import csv
+import os
+
 from typing import List
+
+from pydcop.commands.distribute import (
+    load_algo_module,
+    load_distribution_module,
+    load_graph_module,
+)
+from pydcop.dcop.yamldcop import load_dcop_from_file
+from pydcop.distribution.yamlformat import load_dist_from_file
 
 logger = logging.getLogger("pydcop.cli.consolidate")
 
@@ -78,24 +89,121 @@ def set_parser(subparsers):
     parser.add_argument("files", type=str, nargs="+", help="file(s)")
 
     parser.add_argument(
-        "--extract",
+        "--solution",
         action="store_true",
         default=False,
-        help="Extract result from a json output file",
+        help="Extract end solution metrics from a json output file",
     )
+
+    parser.add_argument(
+        "--distribution_cost",
+        type=str,
+        default=None,
+        help="Distribution file",
+    )
+
+    parser.add_argument(
+        "--algo",
+        type=str,
+        default=None,
+        help="DCOP algorithm",
+    )
+
+
+    parser.add_argument(
+        "--average",
+        action="store_true",
+        default=False,
+        help="compute average result from a json output file",
+    )
+
+    parser.add_argument(
+        "--replace_output",
+        action="store_true",
+        default=False,
+        help="Replace output file instead of appending",
+    )
+
 
 
 def run_cmd(args):
 
-    if args.extract:
+    if args.output and args.replace_output:
+        if os.path.exists(args.output):
+            os.remove(args.output)
+
+    if args.solution:
         files = args.files
         if args.output:
-            # TODO: init file with headers
+            if not os.path.exists(args.output):
+                with open(args.output, mode="w") as output_file:
+                    csv_writer = csv.writer(output_file)
+                    csv_writer.writerow(
+                        ["time", "cost", "cycle", "msg_count", "msg_size", "status"]
+                    )
             with open(args.output, mode="a", newline="") as output_file:
                 extract(files, output_file)
         else:
             target = extract(files, WriterTarget())
             print(target.writen)
+    elif args.distribution_cost:
+        files = args.files
+        if args.output:
+            if not os.path.exists(args.output):
+                with open(args.output, mode="w") as output_file:
+                    csv_writer = csv.writer(output_file)
+                    csv_writer.writerow(["dcop", "distribution", "cost", "hosting", "communication"])
+            with open(args.output, mode="a", newline="") as output_file:
+                distribution_cost(files, args.distribution_cost, args.algo,  output_file)
+        else:
+            target = distribution_cost(files, args.distribution_cost, args.algo, WriterTarget())
+            print("dcop, distrib, cost, hosting, communication")
+            print(target.writen)
+
+
+def distribution_cost(
+    dcop_files: List[str], distribution_file, algo, target
+):
+    logger.debug(f"analyse file {dcop_files}")
+
+    dcop = load_dcop_from_file(dcop_files)
+    path_glob = os.path.abspath(os.path.expanduser(distribution_file))
+    distribution_files = sorted(glob.iglob(path_glob))
+    for distribution_file in distribution_files:
+
+        try:
+            cost, comm, hosting = single_distrib_costs(
+                dcop, distribution_file, algo
+            )
+
+            csv_writer = csv.writer(target)
+            csv_writer.writerow([dcop_files[0], distribution_file, cost, hosting, comm])
+        except:
+            pass
+    return target
+
+
+def single_distrib_costs(dcop, distribution_file, algo):
+    # load files
+    distribution = load_dist_from_file(distribution_file)
+
+    # load modules
+    algo_module = load_algo_module(algo)
+    dist_module = load_distribution_module("ilp_compref")
+    graph_module = load_graph_module(algo_module.GRAPH_TYPE)
+
+    cg = graph_module.build_computation_graph(dcop)
+    computation_memory = algo_module.computation_memory
+    communication_load = algo_module.communication_load
+
+    cost, comm, hosting = dist_module.distribution_cost(
+        distribution,
+        cg,
+        dcop.agents.values(),
+        computation_memory=computation_memory,
+        communication_load=communication_load,
+    )
+    return cost, comm, hosting
 
 
 def extract(files: List[str], target):
